dd906a91c6271ae335b48a94f49a0d93594cb272
tsvetkova2019 <tsvetkova@cyber-host.net>
2019-05-13 00:18:03 +0300

    Реализация сохранения списка книг

--- Form1.cs
+++ Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace Biblioteque {
     public partial class Form1 : Form {
@@ -15,6 +17,7 @@ namespace Biblioteque {
 
         public Form1() {
             InitializeComponent();
+            LoadBooks();
             dataGridView1.DataSource = Books;
         }
 
@@ -29,6 +32,7 @@ namespace Biblioteque {
             using (BookForm dlg = new BookForm(book)) {
                 if (dlg.ShowDialog() == DialogResult.OK) {
                     Books.Add(book);
+                    SaveBooks();
                 }
             }
         }
@@ -42,6 +46,7 @@ namespace Biblioteque {
                 Book book = (Book)dataGridView1.SelectedRows[0].DataBoundItem;
                 using (BookForm dlg = new BookForm(book)) {
                     if (dlg.ShowDialog() == DialogResult.OK) {
+                        SaveBooks();
                         Books.ResetBindings();
                     }
                 }
@@ -57,6 +62,36 @@ namespace Biblioteque {
                 Book book = (Book)dataGridView1.SelectedRows[0].DataBoundItem;
                 if (MessageBox.Show("Удалить книгу?", "Внимание!", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes) {
                     Books.Remove(book);
+                    SaveBooks();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Сохранение списка книг
+        /// </summary>
+        private void SaveBooks() {
+            using (FileStream fs = new FileStream("books.xml", FileMode.Create)) {
+                XmlSerializer serializer = new XmlSerializer(typeof(Book[]));
+                serializer.Serialize(fs, Books.ToArray());
+            }
+        }
+
+        /// <summary>
+        /// Загрузка списка книг
+        /// </summary>
+        private void LoadBooks() {
+            Books.Clear();
+            if (File.Exists("books.xml")) {
+                using (FileStream fs = new FileStream("books.xml", FileMode.Open)) {
+                    XmlSerializer serializer = new XmlSerializer(typeof(Book[]));
+                    object o = serializer.Deserialize(fs);
+                    if (o != null) {
+                        Book[] books = (Book[])o;
+                        foreach (Book book in books) {
+                            Books.Add(book);
+                        }
+                    }
                 }
             }
         }
